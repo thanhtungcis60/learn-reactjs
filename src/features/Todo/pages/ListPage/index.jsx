8362e724a1e32064070504aaebf425f7c9abe639
tungtt <thanhtungcis60@gmail.com>
2025-06-02 17:02:25 +0700

    S9-P57. Sync state to URL

--- src/features/Todo/pages/ListPage/index.jsx
+++ src/features/Todo/pages/ListPage/index.jsx
@@ -1,6 +1,6 @@
-import { use, useState } from 'react';
+import { use, useEffect, useMemo, useState } from 'react';
 import TodoList from '../../components/TodoList';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom/cjs/react-router-dom.min';
 import queryString from 'query-string';
 
 ListPage.propTypes = {
@@ -27,6 +27,8 @@ function ListPage(props) {
     ];
 
     const location = useLocation();
+    const history = useHistory();
+    const match = useRouteMatch();
     const [todoList, setTodoList] = useState(initTodoList);
     const [filteredStatus, setFilteredStatus] = useState(() => {
         //Truy cập đường dẫn http://localhost:3000/todos?status=completed để test
@@ -37,9 +39,7 @@ function ListPage(props) {
     });
 
     const handleTodoClick = (todo, idx) => {
-        //clone current array to the new one
-        const newTodoList = [...todoList];//... Clone
-        // console.log(todo, idx);
+        const newTodoList = [...todoList];//... clone current array to the new one
         //toggle state
         newTodoList[idx] = { ...newTodoList[idx], status: newTodoList[idx].status === 'new' ? 'completed' : 'new' }
         // newTodoList[idx] = newTodo;
@@ -47,18 +47,43 @@ function ListPage(props) {
         //update todo list
         setTodoList(newTodoList);
     }
+    useEffect(() => {
+        // console.log('location change', location.search);
+        const params = queryString.parse(location.search);
+        setFilteredStatus(params.status || 'all'); //update filtered status
+
+    }, [location.search]); //listen to location change
 
     const handleShowAllClick = () => {
-        setFilteredStatus('all');
+        // setFilteredStatus('all');
+        const queryParams = { status: 'all' };
+        history.push({
+            pathname: match.path,
+            search: queryString.stringify(queryParams) //convert object to query string
+        });
     }
     const handleShowCompletedClick = () => {
-        setFilteredStatus('completed');
+        // setFilteredStatus('completed');
+        const queryParams = { status: 'completed' };
+        history.push({
+            pathname: match.path,
+            search: queryString.stringify(queryParams) //convert object to query string
+        });
     }
     const handleShowNewClick = () => {
-        setFilteredStatus('new');
+        // setFilteredStatus('new');
+        const queryParams = { status: 'new' };
+        history.push({
+            pathname: match.path,
+            search: queryString.stringify(queryParams) //convert object to query string
+        });
     }
 
-    const renderedTodoList = todoList.filter(todo => filteredStatus === 'all' || filteredStatus === todo.status);
+    const renderedTodoList = useMemo(() => {
+        //filter todo list based on filteredStatus
+        return todoList.filter(todo => filteredStatus === 'all' || filteredStatus === todo.status);
+    }, [todoList, filteredStatus]); //re-render when todoList or filteredStatus changes
+
     // console.log(renderedTodoList);
     return (
         <div>
