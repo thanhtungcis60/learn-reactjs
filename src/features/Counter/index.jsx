b324f3e0a2417cf7b8df4e424e367d77acd2e011
tungtt <thanhtungcis60@gmail.com>
2025-06-14 11:42:32 +0700

    S14-P90. Material Styles

--- src/features/Counter/index.jsx
+++ src/features/Counter/index.jsx
@@ -1,7 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 import { increment,decrement } from "./counterSlice";
 import styles from "./styles.module.css"; 
+import { makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', // Màu nền gradient
+    border: 0, // Không có viền
+    borderRadius: 3, // Bo tròn góc
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', // Đổ bóng
+    color: 'white', // Màu chữ
+    height: 32, // Chiều cao
+    padding: '0 30px', // Khoảng đệm (padding)
+  },
+});
 
 function CounterFeature() {
     const dispatch = useDispatch();
@@ -16,12 +28,14 @@ function CounterFeature() {
         dispatch(action);
     }
     
+    //css
+    const classes = useStyles();
     return (
         <div className={styles.count}>
             Counter {count.value}
             <div>
-                <button onClick={handleIncreaseClick}>Increase</button>
-                <button onClick={handleDecreaseClick}>Decrease</button>
+                <button className={classes.root} onClick={handleIncreaseClick}>Increase</button>
+                <button className={classes.root} onClick={handleDecreaseClick}>Decrease</button>
             </div>
         </div>
     );
