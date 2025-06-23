83ff0f06efbd719b8d54061407130960ef6f5aac
tungtt <thanhtungcis60@gmail.com>
2025-06-23 11:28:17 +0700

    S15-P94. Tạo dialog để hiển thị form đăng ký

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -16,16 +16,16 @@
 
 // export default Header;
 
-import * as React from 'react';
+import { Dialog, DialogActions, DialogContent, DialogContentText } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/core/Menu';
-import { makeStyles } from '@material-ui/core/styles';
 import CodeIcon from '@material-ui/icons/Code';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,10 +37,28 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  link:{
+    textDecoration: 'none',
+    color: '#fff',
+  }
 }));
 
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    // Nếu reason là 'backdropClick', 'escapeKeyDown', không làm gì cả (không đóng Dialog)
+    if (reason === 'backdropClick'|| reason === 'escapeKeyDown') {
+      return;
+    }
+    setOpen(false);
+  };
+
     const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -49,14 +67,32 @@ export default function Header() {
         <Toolbar>
           <CodeIcon className={classes.menuButton}/>
           <Typography variant="h6" className={classes.title}>
-            EZ Shop
+            <Link className={classes.link} to="/">EZ Shop</Link>
           </Typography>
-          <Button color="inherit">Todos</Button>
-          <Button color="inherit">Albums</Button>
-          <Button color="inherit">Regeister</Button>
+          <NavLink className={classes.link} to="/todos"><Button color="inherit">Todos</Button></NavLink>
+          <NavLink className={classes.link} to="/albums"><Button color="inherit">Albums</Button></NavLink>
+          <Button color="inherit" onClick={handleClickOpen}>Regeister</Button>
         </Toolbar>
       </AppBar>
     </Box>
+
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogContent>
+        <DialogContentText>
+          To subscribe to this website, please enter your email address here. We will send updates occasionally.
+        </DialogContentText>
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
     </div>
   );
 }
