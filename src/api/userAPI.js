410111c2f2ef348aab750d2ae3c8f59644dd041c
tungtt <thanhtungcis60@gmail.com>
2025-06-25 16:16:20 +0700

    S15-P103. Setup userSlice

---
+++ src/api/userAPI.js
@@ -0,0 +1,9 @@
+import axiosClient from './axiosClient';
+
+const userApi = {
+  add(data) {
+    const url = '/auth/local/register';
+    return axiosClient.post(url, data);
+  },
+};
+export default userApi;
