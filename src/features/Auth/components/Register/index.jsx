6eb8a81b3bff92a1c6d1c5a58537ebf969f74cfa
tungtt <thanhtungcis60@gmail.com>
2025-07-04 17:15:42 +0700

    S15-P106. Fix lỗi import package từ node_modules ra sai link

--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import RegisterForm from '../RegisterForm';
-import { unwrapResult } from '@reduxjs/toolkit';
 import { register } from 'features/Auth/userSlice';
+import { unwrapResult } from '@reduxjs/toolkit';
 
 
 
