3433eaf5f27e3fe478b37ed4714f388fc9991f71
tungtt <thanhtungcis60@gmail.com>
2025-07-29 16:17:37 +0700

    S16-P127. Xử lý thumbnail cho sản phẩm

---
+++ src/constants/common.js
@@ -0,0 +1,4 @@
+export const STATIC_HOST = 'https://json-server-na4j.onrender.com';
+export const THUMBNAIL_PLACEHOLDER = 'https://placehold.co/444x444';
+export const PRODUCT_DEFAULT_PLACEHOLDER =
+  'https://cdn.prod.website-files.com/64022de562115a8189fe542a/6616718fe4a871d7278a2037_Product-Concept-What-Is-It-And-How-Can-You-Best-Use-It.jpg';
--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -2,15 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
+import { PRODUCT_DEFAULT_PLACEHOLDER, THUMBNAIL_PLACEHOLDER } from 'constants/index';
 
 Product.propTypes = {
     product: PropTypes.object,
 };
 
 function Product({ product }) {
+    const thumbnailUrl = product.thumbnail ? product.thumbnail : THUMBNAIL_PLACEHOLDER;
     return (
         <Box padding={1}>
-            <Skeleton variant="rect" width="100%" height={118} />
+            {/* <Skeleton variant="rect" width="100%" height={118} /> */}
+            <Box padding={1}>
+                <img
+                    src={thumbnailUrl}
+                    alt={product.name}
+                    style={{
+                        width: '100%',
+                        height: '150px', // hoặc 200px, tuỳ bạn
+                    }}
+                    onError={(e) => {
+                        e.target.onerror = null; // Ngăn gọi lặp vô hạn nếu ảnh fallback cũng lỗi
+                        e.target.src = PRODUCT_DEFAULT_PLACEHOLDER; // Ảnh mặc định bạn đã có
+                    }}
+                />
+            </Box>
             <Typography variant="body2">{product.name}</Typography>
             <Typography variant="body2">{product.salePrice}-{product.promotionPercent}</Typography>
         </Box>
