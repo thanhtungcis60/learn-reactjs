a4f5f6470d90ac4a9aa2bd3b7f9adfac3aa7208f
tungtt <thanhtungcis60@gmail.com>
2025-08-07 16:14:43 +0700

    S17-P144. Setup layout cho trang chi tiết sản phẩm

--- src/features/Product/index.jsx
+++ src/features/Product/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch, useRouteMatch } from 'react-router-dom/cjs/react-router-dom.min';
 import ListPage from './pages/ListPage';
 import { Box } from '@material-ui/core';
+import DetailPage from './pages/DetailPage';
 
 ProductFeature.propTypes = {
 
 };
 
 function ProductFeature(props) {
+    const match = useRouteMatch();
     return (
         <Box pt={4}>
             <Switch>
-                <Route path="/products" exact component={ListPage} />
+                <Route path={match.url} exact component={ListPage} />
+                <Route path={`${match.url}/:productId`} exact component={DetailPage} />
             </Switch>
         </Box>
     );
